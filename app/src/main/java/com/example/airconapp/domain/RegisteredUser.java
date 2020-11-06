ae822eb4a5c0d5e37d7c17d2ad4da45676f87c45
Diogenis <dio.tsolakou@hotmail.com>
2020-11-06 02:38:40 +0200

    Removed priority since it won't be implemented.

--- app/src/main/java/com/example/airconapp/domain/RegisteredUser.java
+++ app/src/main/java/com/example/airconapp/domain/RegisteredUser.java
@@ -6,18 +6,10 @@ public class RegisteredUser extends User
 {
     private String name;
     private String password;
-    private String priority;
-    private static String priorityTooltip;
 
     public RegisteredUser(String name, String password) {
         this.name = name;
         this.password = password;
-    }
-
-    RegisteredUser(String name, String password, String priority) {
-        this.name = name;
-        this.password = password;
-        this.priority = priority;
     }
 
     public String getName() {
@@ -28,20 +20,12 @@ public class RegisteredUser extends User
         return password;
     }
 
-    public String getPriority() {
-        return priority;
-    }
-
     public void setName(String name) {
         this.name = name;
     }
 
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    public void setPriority(String priority) {
-        this.priority = priority;
     }
 
     @Override
