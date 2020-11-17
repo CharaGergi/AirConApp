02c3b0af268cf6ecc3877564bbf1e76f5e504b94
Diogenis <dio.tsolakou@hotmail.com>
2020-11-18 00:56:10 +0200

    Created AdvanceACSettingsActivity and made some graphical implementations in the menu, profile and advanced_ac_settings.

---
+++ app/src/main/java/com/example/airconapp/activities/AdvancedACSettingsActivity.java
@@ -0,0 +1,15 @@
+package com.example.airconapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.airconapp.R;
+
+public class AdvancedACSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_advanced_ac_settings);
+    }
+}
