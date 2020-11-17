d5a64b07dec655f4db9e15c142a0068cf76eaffd
Diogenis <dio.tsolakou@hotmail.com>
2020-11-17 15:50:31 +0200

    Minor graphical implementations in main screen. Added attribute line in manifest for landscape orientation.

--- app/src/main/java/com/example/airconapp/activities/AirConActivity.java
+++ app/src/main/java/com/example/airconapp/activities/AirConActivity.java
@@ -2,6 +2,7 @@ package com.example.airconapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +17,8 @@ public class AirConActivity extends AppCompatActivity implements View.OnClickLis
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_air_con);
+
+        //setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE); // if the attribute in the manifest doesn't work, use this
 
         backBtn = findViewById(R.id.back_button);
         backBtn.setOnClickListener(this);
